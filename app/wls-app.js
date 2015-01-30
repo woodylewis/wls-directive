e2bcae6af5fa06ca5fe10384757393949066746b
Woody Lewis <woody@woodylewis.com>
2015-01-30 14:48:34 -0500

    Move D3 library into a service, draw both bar charts from one directive

--- app/wls-app.js
+++ app/wls-app.js
@@ -4,22 +4,97 @@
 angular.module('wlsApp', [
 	'ui.router',
 	'ui.bootstrap',
-	'ui.bootstrap.tpls'
+	'ui.bootstrap.tpls',
+	'd3'
 ])
-.controller('wlsController', ['$scope', function($scope) {
+.controller('wlsController', ['$scope', 'd3Service', function($scope, d3Service) {
 	$scope.alpha = { range: [4, 8, 15, 42, 23, 16 ], two: 'Data Set 1' };
 	$scope.beta = { range: [8, 3, 19, 16, 7, 26 ], two: 'Data Set 2' };
 }])
-.directive('wlsBarchart1', function() {
+.directive('wlsBarchart1', ['d3Service', function(d3Service) {
 	return {
 		restrict: 'E',
 		scope: {
 			handle1:'=',
+			handle2:'=',
 		},
 		templateUrl: 'templates/wls-chart1.html',
 
 		link: function($scope, element, attrs) {
-			var data = $scope.handle1.range;
+			d3Service.d3().then(function(d3) {
+
+			var data1 = $scope.handle1.range;
+			
+			var width = 420,
+			    barHeight = 20;
+
+			var x = d3.scale.linear()
+			    .domain([0, d3.max(data1)])
+			    .range([0, width]);
+
+			var chart = d3.select(".wls-chart1")
+			    .attr("width", width)
+			    .attr("height", barHeight * data1.length);
+
+			var bar = chart.selectAll("g")
+			    .data(data1)
+			  .enter().append("g")
+			    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+			bar.append("rect")
+			    .attr("width", x)
+			    .attr("height", barHeight - 1);
+
+			bar.append("text")
+			    .attr("x", function(d) { return x(d) - 16; })
+			    .attr("y", barHeight / 2)
+			    .attr("dy", ".35em")
+			    .text(function(d) { return d; });
+
+			var data2 = $scope.handle2.range;
+			
+			var width = 420,
+			    barHeight = 20;
+
+			var x = d3.scale.linear()
+			    .domain([0, d3.max(data2)])
+			    .range([0, width]);
+
+			var chart = d3.select(".wls-chart2")
+			    .attr("width", width)
+			    .attr("height", barHeight * data2.length);
+
+			var bar = chart.selectAll("g")
+			    .data(data2)
+			  .enter().append("g")
+			    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+			bar.append("rect")
+			    .attr("width", x)
+			    .attr("height", barHeight - 1);
+
+			bar.append("text")
+			    .attr("x", function(d) { return x(d) - 16; })
+			    .attr("y", barHeight / 2)
+			    .attr("dy", ".35em")
+			    .text(function(d) { return d; });
+			});
+		}
+	};
+}])
+/*
+.directive('wlsBarchart2', ['d3Service', function(d3Service) {
+	return {
+		restrict: 'E',
+		scope: {
+			handle2:'=',
+		},
+		templateUrl: 'templates/wls-chart2.html',
+
+		link: function($scope, element, attrs) {
+			d3Service.d3().then(function(d3) {
+
+			var data = $scope.handle2.range;
 			
 			var width = 420,
 			    barHeight = 20;
@@ -46,45 +121,7 @@ angular.module('wlsApp', [
 			    .attr("y", barHeight / 2)
 			    .attr("dy", ".35em")
 			    .text(function(d) { return d; });
+			});
 		}
 	};
-})
-.directive('wlsBarchart2', function() {
-	return { 
-		restrict: 'E',
-		scope: {
-			handle2:'=',
-		},
-		templateUrl: 'templates/wls-chart2.html',
-
-		link: function($scope, element, attrs) {
-			var data = $scope.handle2.range;
-			
-			var width = 420,
-			    barHeight = 20;
-
-			var x = d3.scale.linear()
-			    .domain([0, d3.max(data)])
-			    .range([0, width]);
-
-			var chart = d3.select(".wls-chart2")
-			    .attr("width", width)
-			    .attr("height", barHeight * data.length);
-
-			var bar = chart.selectAll("g")
-			    .data(data)
-			  .enter().append("g")
-			    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-			bar.append("rect")
-			    .attr("width", x)
-			    .attr("height", barHeight - 1);
-
-			bar.append("text")
-			    .attr("x", function(d) { return x(d) - 16; })
-			    .attr("y", barHeight / 2)
-			    .attr("dy", ".35em")
-			    .text(function(d) { return d; });
-		}
-	};
-});
+}])*/;
